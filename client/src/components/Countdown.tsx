292598dded3aa217b8b1a31b573f4be23aac173d
bitionaire <johann.boehler@scrumlr.io>
2021-09-17 20:37:04 +0200

    Extracted Countdown component

---
+++ client/src/components/Countdown.tsx
@@ -0,0 +1,86 @@
+import {useStore} from "../state";
+import React, {useEffect, useRef, useState} from "react";
+import classNames from "classnames";
+import {CSSTransition} from "react-transition-group";
+import './Countdown.scss';
+
+const zeroPad = (value: number) => String(value).padStart(2, '0');
+const getRemainingSeconds = (endTime?: Date) => {
+    if (!endTime) {
+        return MAX_COUNTER;
+    }
+
+    const now = new Date();
+    const seconds = Math.floor((endTime.getTime() - now.getTime()) / 1000);
+
+    if (seconds < 0) {
+        return MAX_COUNTER;
+    }
+    return seconds > 0 ? seconds : 0;
+}
+
+const MAX_COUNTER = -30;
+
+const Countdown = () => {
+    const timerEndTime = useStore(state => state.timerEndTime);
+
+    const [counter, setCounter] = useState(getRemainingSeconds(timerEndTime));
+    const [showCountdown, setShowCountdown] = useState( counter > MAX_COUNTER);
+    const ref = useRef(timerEndTime);
+    const nodeRef = useRef(null)
+
+    useEffect(() => {
+        if (ref.current !== timerEndTime) {
+            ref.current = timerEndTime;
+            setShowCountdown(false);
+            if (timerEndTime && getRemainingSeconds(timerEndTime) > MAX_COUNTER) {
+                const timer = setInterval(() => {
+                    setCounter(getRemainingSeconds(timerEndTime));
+                    setShowCountdown(true);
+                }, 3000);
+                return () => clearInterval(timer);
+            }
+        }
+
+        if (counter > MAX_COUNTER) {
+            const timer = setInterval(() => {
+                const newCounter = counter - 1;
+                setCounter(newCounter);
+                if (newCounter <= MAX_COUNTER) {
+                    setShowCountdown(false);
+                }
+            }, 1000);
+            return () => clearInterval(timer);
+        }
+        return () => {};
+    }, [timerEndTime, counter]);
+
+    const minutes = zeroPad(Math.floor(counter / 60));
+    const seconds = zeroPad(counter % 60);
+
+    const text = counter >= 0 ? `${minutes}:${seconds}` : 'any second';
+
+    return (
+        <CSSTransition
+            classNames={{
+                enter: 'countdown--enter',
+                exit: 'countdown--exit',
+            }}
+            className={classNames('countdown', {
+                'countdown--text-fade-in': counter < 1,
+                'countdown--text-faded-in': counter < 0
+            })}
+            in={showCountdown}
+            timeout={{
+                enter: 0, // mount instantly
+                exit: 1000 // unmount once css animation is finished
+            }}
+            nodeRef={nodeRef}
+            unmountOnExit
+        >
+            <div ref={nodeRef}>{text}</div>
+        </CSSTransition>
+    );
+}
+
+export default Countdown;
--- client/src/routes/WaitingScene.tsx
+++ client/src/routes/WaitingScene.tsx
@@ -1,95 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { FC } from 'react';
 import './WaitingScene.scss';
-import {useStore} from "../state";
-import classNames from 'classnames';
 import Background from "../components/Background";
-
-const zeroPad = (value: number) => String(value).padStart(2, '0');
-const getRemainingSeconds = (endTime?: Date) => {
-    if (!endTime) {
-        return MAX_COUNTER;
-    }
-
-    const now = new Date();
-    const seconds = Math.floor((endTime.getTime() - now.getTime()) / 1000);
-
-    if (seconds < 0) {
-        return MAX_COUNTER;
-    }
-    return seconds > 0 ? seconds : 0;
-}
-
-const MAX_COUNTER = -30;
+import Countdown from "../components/Countdown";
 
 export interface WaitingSceneProps {
     label: string;
 }
 
-const WaitingScene: React.FC<WaitingSceneProps> = ({ label}) => {
-    const timerEndTime = useStore(state => state.timerEndTime);
-
-    const [counter, setCounter] = useState(getRemainingSeconds(timerEndTime));
-    const [showCountdown, setShowCountdown] = useState( counter > MAX_COUNTER);
-    const ref = useRef(timerEndTime);
-    const nodeRef = useRef(null)
-
-    useEffect(() => {
-        if (ref.current !== timerEndTime) {
-            ref.current = timerEndTime;
-            setShowCountdown(false);
-            if (timerEndTime && getRemainingSeconds(timerEndTime) > MAX_COUNTER) {
-                const timer = setInterval(() => {
-                    setCounter(getRemainingSeconds(timerEndTime));
-                    setShowCountdown(true);
-                }, 3000);
-                return () => clearInterval(timer);
-            }
-        }
-
-        if (counter > MAX_COUNTER) {
-            const timer = setInterval(() => {
-                const newCounter = counter - 1;
-                setCounter(newCounter);
-                if (newCounter <= MAX_COUNTER) {
-                    setShowCountdown(false);
-                }
-            }, 1000);
-            return () => clearInterval(timer);
-        }
-        return () => {};
-    }, [timerEndTime, counter]);
-
-    const minutes = zeroPad(Math.floor(counter / 60));
-    const seconds = zeroPad(counter % 60);
-
-    const text = counter >= 0 ? `${minutes}:${seconds}` : 'any second';
-
+const WaitingScene: FC<WaitingSceneProps> = ({ label}) => {
     return (
         <div className="waiting-scene">
             <Background />
 
             <main className="waiting-scene__main">
                 <span className="waiting-scene__label">{label}</span>
-                <CSSTransition
-                    classNames={{
-                        enter: 'waiting-scene__countdown--enter',
-                        exit: 'waiting-scene__countdown--exit',
-                    }}
-                    className={classNames('waiting-scene__countdown', {
-                        'waiting-scene__countdown--text-fade-in': counter < 1,
-                        'waiting-scene__countdown--text-faded-in': counter < 0
-                    })}
-                    in={showCountdown}
-                    timeout={{
-                        enter: 0, // mount instantly
-                        exit: 1000 // unmount once css animation is finished
-                    }}
-                    nodeRef={nodeRef}
-                    unmountOnExit
-                >
-                    <div ref={nodeRef}>{text}</div>
-                </CSSTransition>
+                <Countdown />
             </main>
         </div>
     );
