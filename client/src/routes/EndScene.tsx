9955812579fcb384c5cc173bd1bca010e9895b29
bitionaire <johann.boehler@scrumlr.io>
2021-09-17 21:41:29 +0200

    Styled end credits

--- client/src/routes/EndScene.tsx
+++ client/src/routes/EndScene.tsx
@@ -1,5 +1,5 @@
 import {FC, useEffect, useState} from 'react';
-import './WaitingScene.scss';
+import './EndScene.scss';
 import Background from "../components/Background";
 
 const EndScene: FC = () => {
@@ -21,16 +21,18 @@ const EndScene: FC = () => {
                 <span className="end-scene__label">The end</span>
             </main>
 
-            <div>
-                <p>THX</p>
-                <p>for tuning in</p>
-                <p>I'm looking forward to the next session with you and hope you enjoyed today's stream.</p>
-                <p>If so, join my lovely followers and don't miss the next one.</p>
+            {followers.length > 0 && (
+                <div className="end-scene__credits">
+                    <div className="end-scene__credits-thx">THX</div>
+                    <div>for tuning in</div>
+                    <p>I'm looking forward to the next session with you and hope you enjoyed today's stream.</p>
+                    <p>If so, join my lovely followers and don't miss the next one.</p>
 
-                <ul>
-                    {followers.map(follower => <li key={follower}>{follower}</li>)}
-                </ul>
-            </div>
+                    <ul>
+                        {followers.map(follower => <li key={follower}>{follower}</li>)}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
